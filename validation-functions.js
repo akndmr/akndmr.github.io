fbf59b7cb9a3a79aa4021c422499b928384bbf3f
Akın Demir <akin.demir@ogr.sakarya.edu.tr>
2017-10-18 04:08:14 +0300

    URL changed

--- validation-functions.js
+++ validation-functions.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
         var bv = $form.data('bootstrapValidator');
 
         // Use Ajax to submit form data
-        var url = 'https://script.google.com/macros/s/AKfycbzy3UKiaESk9y2ccnBkdSgCo1zxJ0Kx0qE0_eKro7QYE5yFKJVH/exec';
+        var url = 'https://script.google.com/macros/u/0/s/AKfycbx62O8H_mngvRVx8ITBWWburl3hquIevHUGC-tXoRVIvWfyUX76/exec';
         var redirectUrl = 'test-success-page.html';
         // show the loading 
         $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));

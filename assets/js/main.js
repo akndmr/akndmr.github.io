38786f36f6916f1f89d6bfe2a7b4d0add613dd63
Akın Demir <akin.demir@ogr.sakarya.edu.tr>
2018-03-02 02:55:00 +0300

    Popvie App project added

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,7 @@ function loadProjectsHTML() {
       myFunction(this);
     }
   };
-  xmlhttp.open("GET", "https://gist.githubusercontent.com/akndmr/2f89333cd2be5c96b9ca36182aac4401/raw/5bf24cf83144d084cbf461609f5cdb013c1db590/projects.html", true);
+  xmlhttp.open("GET", "https://gist.githubusercontent.com/akndmr/2f89333cd2be5c96b9ca36182aac4401/raw/77b68b355844fa2fc0de5cd6c9c54ea47b80c65b/projects.html", true);
   xmlhttp.send();
 }
 

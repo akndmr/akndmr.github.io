98d0752b9ed8b31879cadc9988d8f4cf6db1e794
Akın Demir <akin.demir@ogr.sakarya.edu.tr>
2018-03-03 06:25:35 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,7 @@ function loadProjectsHTML() {
       myFunction(this);
     }
   };
-  xmlhttp.open("GET", "https://gist.githubusercontent.com/akndmr/2f89333cd2be5c96b9ca36182aac4401/raw/ede3824abab9c8a7fd2768bef53b4f0ddb66d155/projects.html", true);
+  xmlhttp.open("GET", "https://gist.githubusercontent.com/akndmr/2f89333cd2be5c96b9ca36182aac4401/raw/12df8e8f26625ed59d81546b108d0f3960767619/projects.html", true);
   xmlhttp.send();
 }
 

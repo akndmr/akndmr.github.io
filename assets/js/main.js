8cef02bdb48f3fd0c55b2f2b6b9b6070e4f22681
Akın Demir <akin.demir@ogr.sakarya.edu.tr>
2018-06-21 21:21:48 +0300

    Update contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ function loadFormHTML() {
       myFunction(this);
     }
   };
-  xmlhttp.open("GET", "https://gist.githubusercontent.com/akndmr/e88aad3398bfc2a82d9396368bd31abb/raw/13c95473ee099e8552b1fb0fe4f0466901f1f166/form.html", true);
+  xmlhttp.open("GET", "https://gist.githubusercontent.com/akndmr/e88aad3398bfc2a82d9396368bd31abb/raw/ad00fa6400a6190f18ee6ba7f256426c69f82437/form.html", true);
   xmlhttp.send();
 }
 
